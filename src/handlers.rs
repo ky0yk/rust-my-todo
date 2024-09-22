c74d16eb5cdbbec584cd9832a2ba88286c073497
ky0yk <40429527+ky0yk@users.noreply.github.com>
2024-09-22 18:12:29 +0900

    ファイル分割

---
+++ src/handlers.rs
@@ -0,0 +1,16 @@
+use std::sync::Arc;
+
+use axum::{extract::Extension, response::IntoResponse, Json};
+use hyper::StatusCode;
+
+use crate::repositories::{CreateTodo, TodoRepository};
+
+
+pub async fn create_todo<T: TodoRepository>(
+    Json(payload): Json<CreateTodo>,
+    Extension(repository): Extension<Arc<T>>,
+) -> impl IntoResponse {
+    let todo = repository.create(payload);
+
+    (StatusCode::CREATED, Json(todo))
+}
